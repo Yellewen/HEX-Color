94e27f1bd64d5bf198b436f59978b0fcfd4da7d2
Yellewen <92746027+Yellewen@users.noreply.github.com>
2022-07-12 14:46:36 +0300

    Fixed

---
+++ code.js
@@ -0,0 +1,17 @@
+function randomColor() {
+    var palette = "ABCDEF1234567890";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += palette[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+function changeColor() {
+    var body = document.getElementsByTagName("body")[0];
+    var button = document.getElementsByTagName("button")[0];
+    var colorNum = document.getElementById("hex");
+    var newColor = randomColor();
+    body.style.backgroundColor = newColor;
+    button.style.backgroundColor = newColor;
+    colorNum.innerHTML = "HEX Color: " + newColor;
+}
